e9c0fe59f7a77883cb7297c0d7f2d5a601adfe6a
saleh <ntegesaleh@gmail.com>
2026-05-31 16:10:25 +0300

    Add OAuth ensureAuth middleware

---
+++ middleware/ensureAuth.js
@@ -0,0 +1,11 @@
+const ensureAuth = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    return res.status(401).json({
+        message: 'Unauthorized - please login'
+    });
+};
+
+module.exports = ensureAuth;
